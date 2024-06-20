fe81d2dfe6ab01fe63818a6ddaa97db0a9fd6923
Aayush Mankar <mankaraayush18@gmail.com>
2024-06-20 13:12:57 +0530

    removed bcrypt

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const User=require('../models/User')
 const { body, validationResult } = require('express-validator');
 
-const bcrypt=require('bcrypt')
+// const bcrypt=require('bcrypt')
 const jwt=require("jsonwebtoken")
 const jwtSecret="MyNameIsAayushMankarandIAmLegend"
 
@@ -21,14 +21,15 @@ router.post('/createuser',[
       return res.status(400).json({errors:errors.array()})
     }
   
-    const salt=await bcrypt.genSalt(10);  //pass upto 10characters
-    let secPassword=await bcrypt.hash(req.body.password,salt) //req.body.password comes from frontend
+    // const salt=await bcrypt.genSalt(10);  //pass upto 10characters
+    // let secPassword=await bcrypt.hash(req.body.password,salt) //req.body.password comes from frontend
 
-  
+//   
     try {
         await User.create({
             name:req.body.name,   //all this bcoz we have to take the input from the user
-            password:secPassword,  //creates secure password
+            // password:secPassword,
+            password,  //creates secure password
             email:req.body.email,
             location:req.body.location,
         })
@@ -57,7 +58,8 @@ async (req,res)=>{
             return res.status(400).json({errors:"Try logging in with correct email credentials"})
         }
         //checks for password
-        const pwdCompare=await bcrypt.compare(req.body.password,userData.password); 
+        // const pwdCompare=await bcrypt.compare(req.body.password,userData.password); 
+        const pwdCompare=(req.body.password === userData.password);
 
         if (!pwdCompare) {
             return res.status(400).json({errors:"Try logging in with correct password credentials"})
