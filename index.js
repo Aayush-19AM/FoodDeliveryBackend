d88936239eecc36f364306f40ae6ca64a6ed586e
Aayush Mankar <mankaraayush18@gmail.com>
2024-06-20 13:34:33 +0530

    modified cors

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express=require('express');
 const mongoDB = require('./db');
+const cors = require('cors');
 const app=express();
 const port=3000;
 
 mongoDB();
 
+app.use(cors());
+
 app.use(express.json())
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3001");
+    res.setHeader("Access-Control-Allow-Origin","*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-With,Content-Type,Accept"
